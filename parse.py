8bf9950dada9e8260f4eab132a391cb70faf7866
Peter Heiss <peter.heiss@uni-muenster.de>
2020-02-16 22:05:24 +0100

    fixed content

--- parse.py
+++ parse.py
@@ -9,17 +9,17 @@ def parse(fg, url):
 	items = soup.find_all("li", {"class": "feed-item"})
 	for item in items:
 		fe = fg.add_entry()
-		fe.title(item.a.contents)
+		fe.title(item.a.contents[0])
 		fe.link(href=item.a["href"])
 
 		try:
 			from datetime import datetime
-			fe.published(datetime.strptime(item.find("span", {"class": "feed-date"}).contents, '%d. %m. %Y'))
+			fe.published(datetime.strptime(item.find("span", {"class": "feed-date"}).contents[0], '%d. %m. %Y'))
 		except:
 			pass
 		
 		try:
-			fe.author(item.find("span", {"class": "feed-source"}).contents)
+			fe.author(item.find("span", {"class": "feed-source"}).contents[0])
 		except:
 			pass
 
